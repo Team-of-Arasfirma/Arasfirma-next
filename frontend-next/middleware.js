df2faf630af3c729484a84b90e3caf220f2b5de1
jothi Prakash <jothiprakash.arasfirma@gmail.com>
2026-08-10 11:13:48 +0530

    Add sitemap uppercase rewrite support

---
+++ frontend-next/middleware.js
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/Sitemap-blog.xml") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/sitemap-blog.xml";
+    return NextResponse.rewrite(url);
+  }
+
+  if (pathname === "/Sitemap.xml") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/sitemap.xml";
+    return NextResponse.rewrite(url);
+  }
+
+  if (pathname === "/Sitemap-pages.xml") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/sitemap-pages.xml";
+    return NextResponse.rewrite(url);
+  }
+
+  if (pathname === "/Sitemap-products.xml") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/sitemap-products.xml";
+    return NextResponse.rewrite(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/Sitemap-blog.xml",
+    "/Sitemap.xml",
+    "/Sitemap-pages.xml",
+    "/Sitemap-products.xml",
+  ],
+};
